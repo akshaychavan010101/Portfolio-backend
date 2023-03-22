aff62ed6ef63c9ec64681513b3af32cf8b1b83f1
Akshay Chavan <115460540+akshaychavan010101@users.noreply.github.com>
2023-03-22 13:15:42 +0530

    update

--- index.js
+++ index.js
@@ -15,42 +15,33 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 app.post("/sendmail", (req, res) => {
   const { name, email, subject, message } = req.body;
-
-  const Mailchecker = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-  if (!name || !email || !subject || !message) {
-    res.send("Please fill all the fields");
-  } else if (!Mailchecker.test(email)) {
-    res.send("Please enter a valid email");
-  } else {
-    sgMail.setApiKey(process.env.SG_API_KEY);
-    const messages = [
-      {
-        to: `akshaychavan010101@gmail.com`,
-        from: "akki010102@gmail.com",
-        subject: subject,
-        html: `
-          <strong>
-             <h2>From: ${name}</h2>
-             <h2> Email: ${email}</h2>
-             <p>${message}<p>
-          </strong>`,
-      },
-    ];
-    sgMail.send(messages).then(
-      () => {
-        res.send("Mail Sent");
-      },
-      (error) => {
-        if (error.response) {
-          res.send({
-            msg: "Something Went Wrong",
-            Error: error.response.body,
-          });
-        }
-      }
-    );
-  }
+  sgMail.setApiKey(process.env.SG_API_KEY);
+  const messages = [
+    {
+      to: `akshaychavan010101@gmail.com`,
+      from: "akki010102@gmail.com",
+      subject: subject,
+      html: `
+        <strong>
+           <h2>From: ${name}</h2>
+           <h2> Email: ${email}</h2>
+           <p>${message}<p>
+        </strong>`,
+    },
+  ];
+  sgMail.send(messages).then((success, error) => {
+    if (error) {
+      res.send({
+        msg: "Something Went Wrong",
+        Error: error.response.body,
+      });
+    }
+    else{
+      res.json({
+        msg: "Mail Sent Successfully",
+      });
+    }
+  });
 });
 
 app.listen(process.env.PORT, () => console.log(`app listening on port ${process.env.PORT}`));
